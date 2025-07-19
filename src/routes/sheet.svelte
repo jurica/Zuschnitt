b2a3d4b6928e085505a562422448fbb15152f42d
Jurica Bacurin <jurica@bacurin.de>
2025-07-19 15:42:19 +0200

    wip

--- src/routes/sheet.svelte
+++ src/routes/sheet.svelte
@@ -1,19 +1,31 @@
+<script lang="ts">
+  function clickedSheet() {
+    console.log("sheet");
+  }
+  function clickedColumn() {
+    console.log("column");
+  }
+  function clickedPart() {
+    console.log("part");
+  }
+</script>
+
 <svg viewBox="0 0 2500 1250">
 <defs>
   <linearGradient x1="0%" y1="0%" x2="100%" y2="100%" spreadMethod="pad" id="sheet" class="text-slate-500">
-        <stop offset="0%" stop-color="currentColor" stop-opacity="0.1"></stop>
-        <stop offset="100%" stop-color="currentColor" stop-opacity="0.25"></stop>
+    <stop offset="0%" stop-color="currentColor" stop-opacity="0.1"></stop>
+    <stop offset="100%" stop-color="currentColor" stop-opacity="0.25"></stop>
   </linearGradient>
   <linearGradient x1="0%" y1="0%" x2="100%" y2="100%" id="column" class="text-cyan-200">
-        <stop offset="0%" stop-color="currentColor" stop-opacity="0.1"></stop>
-        <stop offset="100%" stop-color="currentColor" stop-opacity="0.25"></stop>
+    <stop offset="0%" stop-color="currentColor" stop-opacity="0.1"></stop>
+    <stop offset="100%" stop-color="currentColor" stop-opacity="0.25"></stop>
   </linearGradient>
   <linearGradient x1="0%" y1="0%" x2="100%" y2="100%" id="part" class="text-violet-500">
-        <stop offset="0%" stop-color="currentColor" stop-opacity="0.1"></stop>
-        <stop offset="100%" stop-color="currentColor" stop-opacity="0.25"></stop>
+    <stop offset="0%" stop-color="currentColor" stop-opacity="0.1"></stop>
+    <stop offset="100%" stop-color="currentColor" stop-opacity="0.25"></stop>
   </linearGradient>
 </defs>
-    <rect x="0" y="0" width="2500" height="1250" stroke-width="1" stroke="black" fill="url(#sheet)"/>
-    <rect x="0" y="0" width="200" height="1250" stroke-width="1" stroke="black" fill="url(#column)"/>
-    <rect x="0" y="0" width="200" height="300" stroke-width="1" stroke="black" fill="url(#part)"/>
+    <rect x="0" y="0" width="2500" height="1250" stroke-width="1" stroke="black" fill="url(#sheet)" onclick={clickedSheet}/>
+    <rect x="0" y="0" width="200" height="1250" stroke-width="2" stroke="black" fill="url(#column)" onclick={clickedColumn}/>
+    <rect x="0" y="50" width="200" height="300" stroke-width="1" stroke="black" fill="url(#part)" onclick={clickedPart}/>
 </svg>
---
+++ src/routes/sheethtml.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button/index.js";
+
+  function clickedSheet() {
+    console.log("sheet");
+  }
+  function clickedColumn() {
+    console.log("column");
+  }
+  function clickedPart() {
+    console.log("part");
+  }
+
+  const formatter = new Intl.NumberFormat("de-DE", {minimumFractionDigits:2, maximumFractionDigits: 2});
+  var scale = $state(0.55);
+  const w = "300";
+</script>
+<div class="flex">
+<Button onclick={() => scale -= 0.05}>-</Button>
+<p>{formatter.format(scale)}</p>
+<Button onclick={() => scale += 0.05}>+</Button>
+</div>
+<div class="relative w-[2500px] h-[1250px] origin-top-left scale-[var(--scale)]" style="--scale: {scale}">
+  <div class="absolute top-0 left-0 w-full h-full border border-black bg-linear-135 from-slate-100 to-slate-300">
+    <div class="absolute w-[var(--w)] h-full border border-black bg-linear-45 from-cyan-50 hover:from-cyan-100 to-cyan-200 hover:to-cyan-300" style="--w: {w}px">
+      <div class="absolute top-0 left-0 w-[var(--w)] h-[300px] border border-black hover:border-red-500 bg-linear-45 from-violet-50 hover:from-violet-100 to-violet-200 hover:to-violet-300"></div>
+      <div class="absolute top-[300px] left-0 w-[var(--w)] h-[300px] border border-black hover:border-red-500 bg-linear-45 from-violet-50 hover:from-violet-100 to-violet-200 hover:to-violet-300"></div>
+    </div>
+    <div class="absolute left-[300px] w-[500px] h-full border border-black bg-linear-45 from-cyan-50 to-cyan-200 hover:to-cyan-300">
+      <div class="absolute top-0 left-0 w-full h-[175px] border border-black hover:border-red-500 bg-linear-45 from-violet-50 hover:from-violet-100 to-violet-200 hover:to-violet-300"></div>
+    </div>
+  </div>
+</div>
+
+<div class="relative w-[2500px] h-[1250px] origin-top-left scale-[var(--scale)]" style="--scale: {scale}">
+  <div class="absolute top-0 left-0 w-full h-full border border-black bg-linear-135 from-slate-100 to-slate-300">
+    <div class="absolute w-[var(--w)] h-full border border-black bg-linear-45 from-cyan-50 hover:from-cyan-100 to-cyan-200 hover:to-cyan-300" style="--w: {w}px">
+      <div class="absolute top-0 left-0 w-[var(--w)] h-[300px] border border-black hover:border-red-500 bg-linear-45 from-violet-50 hover:from-violet-100 to-violet-200 hover:to-violet-300"></div>
+      <div class="absolute top-[300px] left-0 w-[var(--w)] h-[300px] border border-black hover:border-red-500 bg-linear-45 from-violet-50 hover:from-violet-100 to-violet-200 hover:to-violet-300"></div>
+    </div>
+    <div class="absolute left-[300px] w-[500px] h-full border border-black bg-linear-45 from-cyan-50 to-cyan-200 hover:to-cyan-300">
+      <div class="absolute top-0 left-0 w-full h-[175px] border border-black hover:border-red-500 bg-linear-45 from-violet-50 hover:from-violet-100 to-violet-200 hover:to-violet-300"></div>
+    </div>
+  </div>
+</div>
